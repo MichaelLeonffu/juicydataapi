619903d07980dc6dec9afdc2c2060296d712612b
Michael Leonffu <leonffu.michael@gmail.com>
2018-07-18 22:51:16 -0700

    Added db name to the config
    tenative; will change pretty soon

--- configTEMPLATE.js
+++ configTEMPLATE.js
@@ -5,7 +5,8 @@
 module.exports = {
 	mongodb:{
 		//mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
-		url: 'mongodb://localhost/'
+		url: 'mongodb://localhost/',
+		db: 'JuicyData'
 	},
 	api:{
 		version: '2.0.0-alpha'
