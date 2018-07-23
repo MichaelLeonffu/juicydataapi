cc636d74cf63cda15793d62f008aeee38487e11e
Michael Leonffu <leonffu.michael@gmail.com>
2018-07-23 00:30:33 -0700

    added tenative to accounts schema

--- api/accounts/schema.js
+++ api/accounts/schema.js
@@ -1,15 +1,31 @@
-//in the "users" collection
+//in 'users' collection
 {
 	_id: ObjectId(), //mongodb unique id of user
 	email: 'abc',
-	passowrd: 'abc'
+	password: 'abc'
 }
 
-//in the "profiles" collection
+//in 'profiles' collection
 {
 	_id: ObjectId(), //same as user id
-	name: {
+	name:{
 		first: 'abc',
 		last: 'abc'
+	}
+}
+
+//in 'tentative' collection
+{
+	_id: ObjectId(), 		//same as user id
+	birthTime: ISODate(),	//time this became tenative
+	user:{
+		email: 'abc',
+		password: 'abc'
+	},
+	profile:{
+		name:{
+			first: 'abc',
+			last: 'abc'
+		}
 	}
 }
